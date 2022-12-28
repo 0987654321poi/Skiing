19f0c0b881424f3026908b751e8b9e05f586266a
Alejandro Vargas Altamirano <aava001@bucknell.edu>
2022-12-28 11:10:15 -0500

    First commit, project setup

---
+++ src/main/java/intro/HelloMain.java
@@ -0,0 +1,25 @@
+/* ******************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Alejandro Vargas Altamirano
+ * Date: 9/8/2022
+ * Time: 1:14 PM
+ *
+ * Project:MyWork
+ * Package:intro
+ * Class: HelloMain
+ *
+ * Description:
+ *
+ ********************************************
+ */
+
+package intro;
+
+public class HelloMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Intellij");
+    }
+}
