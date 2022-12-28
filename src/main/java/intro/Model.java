6191174be544b98681380959f5f29864a69ced58
Alejandro Vargas Altamirano <aava001@bucknell.edu>
2022-12-28 11:27:51 -0500

    Setting up javafx in gradle and mvc

---
+++ src/main/java/intro/Model.java
@@ -0,0 +1,22 @@
+/* ******************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Alejandro Vargas Altamirano
+ * Date: 12/28/2022
+ * Time: 11:24 AM
+ *
+ * Project:MyWork
+ * Package:intro
+ * Class: Model
+ *
+ * Description:
+ *
+ ********************************************
+ */
+
+package intro;
+
+public class Model {
+}
