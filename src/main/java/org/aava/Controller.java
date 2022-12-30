405bc01e41dad64b1154dde3180a298847e4ebc6
Alejandro Vargas Altamirano <aava001@bucknell.edu>
2022-12-30 11:45:02 -0500

    Added some basic documentation

--- src/main/java/org/aava/Controller.java
+++ src/main/java/org/aava/Controller.java
@@ -35,34 +35,75 @@ import java.util.ResourceBundle;
 
 
 public class Controller implements Initializable {
+    /**
+     * Reference to the model(may not be needed as this game is quite simple)
+     */
     private Model theModel;
 
+    /**
+     * AnimationTimer for the skiing game
+     */
     AnimationTimer gameLoop;
 
+    /**
+     * Keeps track of the time the game has been running
+     */
     private int time = 0;
 
+    /**
+     * Boolean property determining if the player has lost yet
+     */
     private boolean lost = false;
 
+    /**
+     * The label in the GUI that displys the score
+     */
     @FXML
     private Label score;
 
+    /**
+     * The skier icon that the player controls
+     */
     @FXML
     private ImageView skier;
 
+    /**
+     * The Pane container that holds the skier and the gates
+     */
     @FXML
     private Pane pane;
 
+    /**
+     * This is the red gate on the left side of the screen that the skier must ski around
+     */
     @FXML
     private Circle l1;
 
+    /**
+     * This is the blue gate on the right side of the screen that the skier must ski around
+     */
     @FXML
     private Circle r1;
 
 
+    /**
+     * Attaches the model to our controller(agian may not be necessary)
+     * @param model
+     */
     public void setModel(Model model){
         this.theModel = model;
     }
 
+    /**
+     * Initializes game, also sets the initial Y location of the gates(l1 and r1)
+     * @param url
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resourceBundle
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
         pane.getChildren().get(1).setLayoutY(500);
@@ -77,6 +118,10 @@ public class Controller implements Initializable {
         gameLoop.start();
     }
 
+    /**
+     * Event handler for when the player tries to control the skier with the arrow keys
+     * @param event
+     */
     @FXML
     public void pressed(KeyEvent event) {
         if(event.getCode() == KeyCode.LEFT)
@@ -85,6 +130,9 @@ public class Controller implements Initializable {
             right();
     }
 
+    /**
+     * Holds the logic for if the player hits the left arrow
+     */
     public void left(){
         if (lost)
             return;
@@ -93,6 +141,10 @@ public class Controller implements Initializable {
         Image skiMan = new Image("leftSkier.png");
         skier.setImage(skiMan);
     }
+
+    /**
+     * Holds the logic for if the skier clicks the right arrow
+     */
     public void right(){
         if (lost)
             return;
@@ -102,6 +154,10 @@ public class Controller implements Initializable {
         skier.setImage(skiMan);
     }
 
+    /**
+     * This method is called in the handle method every frame and updates the view with the
+     * appropriate changes
+     */
     private void update(){
         time++;
         List<Node> temp = pane.getChildren();
